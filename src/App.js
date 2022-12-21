d4d3ac3865e8cb73b1ed58fa4d1f3ba3d54a8be9
alexandre gvozdenovic <alexandre.gvozdenovic@gmail.com>
2022-12-21 17:02:47 +0100

    first and last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+
+const StyledContainer = styled("div")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledImg = styled("img")`
+  width: 200px;
+  height: auto;
+`;
+
+const StyledInput = styled("input")`
+  margin-bottom: 32px;
+`;
 
 function App() {
+  const [wines, setWines] = useState([]);
+  const [filteredWines, setFilteredWines] = useState([]);
+  const [search, setSearch] = useState("");
+
+  console.log("wines", wines);
+  const getData = async () => {
+    const data = await Axios.get("/wines");
+    setWines(data.data.data);
+    setFilteredWines(data.data.data);
+    console.log("DATA", data);
+  };
+
+  const searchFilter = (search) => {
+    console.log("search", search);
+    const filtered = wines.filter((wine) =>
+      wine.name.toLowerCase().includes(search.toLowerCase())
+    );
+    console.log(filtered);
+    setFilteredWines(filtered);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledContainer className="App">
+      <h1>El vino</h1>
+      <StyledInput onChange={(e) => searchFilter(e.target.value)} />
+      {filteredWines.map((wine) => {
+        return (
+          <div>
+            <p>{wine.name}</p>
+            <StyledImg src={wine.logo} />
+          </div>
+        );
+      })}
+    </StyledContainer>
   );
 }
 
